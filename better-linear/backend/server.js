23f5072b444c56b2b928e39728d236dc7ccd123e
Manav Patel <manav.patel@mail.utoronto.ca>
2022-03-07 15:55:38 -0500

    More dependencies

--- better-linear/backend/server.js
+++ better-linear/backend/server.js
@@ -23,7 +23,7 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true,
 }));
 app.listen(5000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+console.log('Running a GraphQL API server at http://localhost:5000/graphql');
 
 // app.get('/', (req, res) => {
 //     res.send('Hello world!');
