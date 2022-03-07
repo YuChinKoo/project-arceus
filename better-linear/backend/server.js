30a1942e1def79faa6d3e4a308577276204888b9
Yu Chin Koo <eugenekoo777@gmail.com>
2022-03-07 15:09:32 -0500

    Added dependencies for backend

---
+++ better-linear/backend/server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = {
+    hello: () => {
+      return 'Hello world!';
+    },
+};
+
+var app = express();
+const port = 5000
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+app.listen(5000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+
+// app.get('/', (req, res) => {
+//     res.send('Hello world!');
+// });
+
+// app.listen(port, () => {
+//     console.log(`example app listening on port ${port}`);
+// });
--- better-linear/src/App.js
+++ better-linear/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>dasd dasd</div>
+      <div>dasd dasdfsdfse</div>
     </div>
   );
 }
