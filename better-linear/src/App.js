4d2490989f0ee3fe20a49033e6233a32ef6ab54c
Manav Patel <manav.patel@mail.utoronto.ca>
2022-03-07 16:41:19 -0500

    Broken graphql
    following https://www.youtube.com/watch?v=YyUWW04HwKY

--- better-linear/src/App.js
+++ better-linear/src/App.js
@@ -1,4 +1,20 @@
 import './App.css';
+import {ApolloClient, 
+        InMemoryCache, 
+        ApolloProvider, 
+        HttpLink, 
+        from,
+} from '@apollo/client'
+
+const link = from([
+  errorLink,
+  new HttpLink({uri: "http://localhost:5000/graphql"}),
+])
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link 
+})
 
 function App() {
   return (
