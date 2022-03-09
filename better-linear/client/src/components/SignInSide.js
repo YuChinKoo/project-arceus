0fcca42087ce1e708ed44398a62b3c2a8127622d
Yuanyuan Li <tuanzi.li@mail.utoronto.ca>
2022-03-08 22:55:50 -0500

    Link in Signin and Signup component

--- better-linear/client/src/components/SignInSide.js
+++ better-linear/client/src/components/SignInSide.js
@@ -5,14 +5,14 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import { Link } from 'react-router-dom';
+import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
+import { Link as RouterLink } from 'react-router-dom';
 
 const theme = createTheme();
 
@@ -96,7 +96,7 @@ export default function SignInSide() {
               </Button>
               <Grid container>
                 <Grid item>
-                  <Link to="/signup" variant="body2">
+                  <Link component={RouterLink} to="/signup" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
--- better-linear/client/src/components/SignUpSide.js
+++ better-linear/client/src/components/SignUpSide.js
@@ -5,13 +5,14 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import { Link } from 'react-router-dom';
+import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Link as RouterLink } from 'react-router-dom';
 
 import gql from "graphql-tag";
 
@@ -131,12 +132,12 @@ export default function SignUpSide() {
               </Button>
               <Grid container>
               <Grid item>
-                <Link to="/signin" className="">
+                {/* <Link to="/signin" className="">
                   Already have an account? Sign In
-                </Link>                
-                {/* <Link to="/signin" variant="body2">
+                </Link>                 */}
+                <Link component={RouterLink} to="/signin" variant="body2">
                   {"Already have an account? Sign In"}
-                </Link> */}
+                </Link>
               </Grid>
             </Grid>
             </Box>
