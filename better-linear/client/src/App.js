b3fdde64794e713a6c4b54f50351a1acbde33e19
Yu Chin Koo <eugenekoo777@gmail.com>
2022-03-11 19:47:50 -0500

    added frontend authorization

--- better-linear/client/src/App.js
+++ better-linear/client/src/App.js
@@ -5,31 +5,64 @@ import SignInSide from './components/SignInSide';
 import SignUpSide from './components/SignUpSide';
 import Homepage from './components/Homepage';
 import './App.css';
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/client";
+
+const GET_CONTEXT = gql`
+  query {
+    me {
+      id
+      firstname
+      lastname
+      email
+    }
+  }
+`
 
 function App() {
-  return (
-    <div className="center w85">
-      <Header />
-      <Homepage/>
-      <div className="ph3 pv1 background-gray">
-        <Routes>
-          <Route 
-            path="/" 
-            element={<SignUpSide/>} 
-          />
-          <Route 
-            path="/signin" 
-            element={<SignInSide/>} 
-          />
-          <Route
-            path="/signup"
-            element={<SignUpSide/>}
-          />
-        </Routes>
+  const { loading, error, data } = useQuery(GET_CONTEXT);
+  
+  if (loading) return "loading";
+  if (error) return `Error! ${error.message}`;
+  if (data.me) {
+    return (
+      <div className="center w85">
+        <Header authorization={true} />
+        <Homepage/>
+        <div className="ph3 pv1 background-gray">
+          <Routes>
+            <Route 
+              path="/" 
+              element={<div>hello</div>} 
+            />
+          </Routes>
+        </div>
       </div>
-
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="center w85">
+        <Header authorization={false} />
+        <div className="ph3 pv1 background-gray">
+          <Routes>
+            <Route 
+              path="/" 
+              element={<SignUpSide/>} 
+            />
+            <Route 
+              path="/signin" 
+              element={<SignInSide/>} 
+            />
+            <Route
+              path="/signup"
+              element={<SignUpSide/>}
+            />
+          </Routes>
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- better-linear/client/src/components/Header.js
+++ better-linear/client/src/components/Header.js
@@ -2,22 +2,38 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
-const Header = () => {
-    return (
-        <div className="">
-            <div className="top-header">
-                <Link to="/" className="no-underline black">
-                    <div className="fw7 mr1">B-Linear</div>
-                </Link>        
-                <Link to="/signin" className="">
-                    Sign In
-                </Link>
-                <Link to="/signup" className="">
-                    Sign Up
-                </Link>
+const Header = (prop) => {
+    if (prop.authorization) {
+        return (
+            <div className="">
+                <div className="top-header">
+                    <Link to="/" className="no-underline black">
+                        <div className="fw7 mr1">B-Linear</div>
+                    </Link>        
+                    <Link to="/signin" className="">
+                        Sign Out
+                    </Link>
+                </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return (
+            <div className="">
+                <div className="top-header">
+                    <Link to="/" className="no-underline black">
+                        <div className="fw7 mr1">B-Linear</div>
+                    </Link>        
+                    <Link to="/signin" className="">
+                        Sign In
+                    </Link>
+                    <Link to="/signup" className="">
+                        Sign Up
+                    </Link>
+                </div>
+            </div>
+        );
+    }
+    
 };
 
 export default Header;
